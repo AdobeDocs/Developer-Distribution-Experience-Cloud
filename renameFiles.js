e5a1c49eaee293c8abfacc162914f5992a0fb4fc
Louisa Chu <louisa@ensemble.com>
2025-04-30 16:05:43 -0700

    update scripts

--- renameFiles.js
+++ renameFiles.js
@@ -1,13 +1,15 @@
 const path = require('path');
 const fs = require('node:fs');
 const { pathPrefix } = require('./gatsby-config.js');
-const { globSync }= require('glob');
-const { readRedirectionsFile, writeRedirectionsFile, getRedirectionsFilePath } = require('./scriptUtils.js');
-
-function getMarkdownFiles() {
-    return globSync(__dirname + '/src/pages/**/*.md')
-        .map(f => path.resolve(f));
-}
+const { 
+    readRedirectionsFile, 
+    writeRedirectionsFile, 
+    getRedirectionsFilePath, 
+    getMarkdownFiles, 
+    getFindPatternForMarkdownFiles,
+    getReplacePatternForMarkdownFiles,
+    replaceLinksInFile 
+} = require('./scriptUtils.js');
 
 function toKebabCase(str) {
     const isScreamingSnakeCase = new RegExp(/^[A-Z0-9_]*$/gm).test(str);
@@ -23,18 +25,13 @@ function toEdsCase(str) {
     return isValid ? str : toKebabCase(str);
 }
 
-function toUrl(file, renameBaseWithoutExt) {
+function toUrl(file, renameBaseWithoutExt = name => name) {
     const base = path.basename(file);
     const ext = path.extname(file);
     const end = file.length - base.length;
     const baseWithoutExt = base.substring(0, base.length - ext.length);
     const newBaseWithoutExt = renameBaseWithoutExt(baseWithoutExt);
     return `${file.substring(0, end)}${newBaseWithoutExt}`
-}
-
-function toRelativeUrl(fromDir, file) {
-    const relativeFile = path.relative(fromDir, file);
-    return toUrl(relativeFile, f => f);
 }
 
 function renameFile(file, renameBaseWithoutExt) {
@@ -57,21 +54,11 @@ function getFileMap(files) {
 function getLinkMap(fileMap, relativeToDir) {
     const linkMap = new Map();    
     fileMap.forEach((toFile, fromFile) => {
-        const fromUrl = toRelativeUrl(relativeToDir, fromFile);
-        const toUrl = toRelativeUrl(relativeToDir, toFile);
-        linkMap.set(fromUrl, toUrl);
+        const fromRelFile = path.relative(relativeToDir, fromFile);
+        const toRelFile = path.relative(relativeToDir, toFile);
+        linkMap.set(fromRelFile, toRelFile);
     });
     return linkMap;
-}
-
-function replaceLinksInFile({ file, linkMap, getFindPattern, getReplacePattern }) {
-    let data = fs.readFileSync(file, 'utf8');
-    linkMap.forEach((to, from) => {
-        const find = getFindPattern(from);
-        const replace = getReplacePattern(to);
-        data = data.replaceAll(new RegExp(find, "gm"), replace);
-    });
-    fs.writeFileSync(file, data, 'utf-8');
 }
 
 function renameLinksInMarkdownFile(fileMap, file) {
@@ -79,8 +66,8 @@ function renameLinksInMarkdownFile(fileMap, file) {
     replaceLinksInFile({ 
         file, 
         linkMap: getLinkMap(fileMap, dir),
-        getFindPattern: (from) => `(\\[[^\\]]*]\\()(${from})(#[^\\()]*)?(\\))`,
-        getReplacePattern: (to) => `$1${to}$3$4`,
+        getFindPattern: getFindPatternForMarkdownFiles,
+        getReplacePattern: getReplacePatternForMarkdownFiles,
     });
 }
 
@@ -90,8 +77,8 @@ function renameLinksInRedirectsFile(fileMap) {
     replaceLinksInFile({
         file,
         linkMap: getLinkMap(fileMap, dir),
-        getFindPattern: (from) => `(['"]?)(Source|Destination)(['"]?\\s*:\\s*['"])(${pathPrefix}${from})(#[^'"]*)?(['"])`,
-        getReplacePattern: (to) => `$1$2$3${pathPrefix}${to}$5$6`,
+        getFindPattern: (from) => `(['"]?)(Source|Destination)(['"]?\\s*:\\s*['"])(${pathPrefix}${toUrl(from)})(/?)(#[^'"]*)?(['"])`,
+        getReplacePattern: (to) => `$1$2$3${pathPrefix}${toUrl(to)}$5$6$7`,
     });
 }
 
@@ -100,8 +87,8 @@ function renameLinksInGatsbyConfigFile(fileMap, file) {
     replaceLinksInFile({
         file,
         linkMap: getLinkMap(fileMap, dir),
-        getFindPattern: (from) => `(['"]?path['"]?\\s*:\\s*['"])(/${from})(#[^'"]*)?(['"])`,
-        getReplacePattern: (to) => `$1/${to}$3$4`,
+        getFindPattern: (from) => `(['"]?path['"]?\\s*:\\s*['"])(/|./)?(${from})(#[^'"]*)?(['"])`,
+        getReplacePattern: (to) => `$1$2${to}$4$5`,
     });
 }
 
@@ -112,8 +99,8 @@ function appendRedirects(fileMap) {
     const newData = [];
     linkMap.forEach((to, from) => {
         newData.push({
-            Source:  `${pathPrefix}${from}`, 
-            Destination: `${pathPrefix}${to}`,
+            Source:  `${pathPrefix}${toUrl(from)}`, 
+            Destination: `${pathPrefix}${toUrl(to)}`,
         })
     });
     const currData = readRedirectionsFile();
